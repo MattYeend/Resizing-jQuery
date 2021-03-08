b7d0ae650b3109684bc98e7c92327eb2e9379a84
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-08 11:47:29 +0000

    Initial Commit

---
+++ Resizing/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".start").click(function(){
+		$("p").animate({width:"100%"}).animate({fontSize: "46px"}).animate({borderWidth: 30});
+	});
+	$(".reset").click(function(){
+		$("p").removeAttr("style");
+	});
+});
